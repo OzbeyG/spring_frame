6a68181a700fa35f4bc49a9820ab158263ee0c8f
Ozbey <gokhanozbey@gmail.com>
2022-02-26 19:21:52 -0500

    Spring-05-Core-BeanScope

---
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/CydeoApp.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+import com.cydeo.config.ProjectConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.repository.CommentRepository;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CydeoApp {
+    public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Mike");
+        comment.setText("Spring Framework");
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService commentService = container.getBean(CommentService.class);
+        CommentRepository commentRepository = container.getBean(CommentRepository.class);
+
+        commentRepository.storeComment(comment);
+        commentService.publishComment(comment);
+
+
+
+    }
+}
